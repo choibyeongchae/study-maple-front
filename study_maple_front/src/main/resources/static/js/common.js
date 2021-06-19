e641f9f0e513a6b7d0e490f037451834f9833c55
BEST <BEST@192.168.0.12>
2021-06-19 17:24:33 +0900

    추가 커밋

---
+++ study_maple_front/src/main/resources/static/js/common.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+class Commonjs {
+
+	constructor()
+	{
+		
+	}
+	
+	isEmpty(value) {
+		if( value == "" || value == null || value == undefined || ( value != null && typeof value == "object" && !Object.keys(value).length ) ){ 
+			return true 
+		}else{ 
+			return false
+		}
+	}
+}
