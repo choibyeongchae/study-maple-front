105f4df5df4c9756dde381188f09c769b9906348
BEST <BEST@192.168.0.12>
2021-06-29 01:03:26 +0900

    homcotroller

--- study_maple_front/src/main/java/com/maple/front/controller/HomeController.java
+++ study_maple_front/src/main/java/com/maple/front/controller/HomeController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	@GetMapping(value = "/index")
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home(Model model) {
 		
-		return "index";
+		return "home";
 	}
 }
