553a112ad4678e9cca4a4fb987d886102418f65d
choibyeongchae <choibyeongchae@localhost>
2021-07-01 23:35:06 +0900

    시스템 Date추가

--- study_maple_front/src/main/java/com/maple/front/StudyMapleFrontApplication.java
+++ study_maple_front/src/main/java/com/maple/front/StudyMapleFrontApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.maple.front.util.UserDetailUtil;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 @EnableCaching
+@EnableJpaAuditing
 @SpringBootApplication
 public class StudyMapleFrontApplication {
 
--- study_maple_front/src/main/java/com/maple/front/util/DateEntityUtil.java
+++ study_maple_front/src/main/java/com/maple/front/util/DateEntityUtil.java
@@ -3,12 +3,18 @@ package com.maple.front.util;
 import java.time.Instant;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.Getter;
 import lombok.Setter;
 
+@MappedSuperclass
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class DateEntityUtil {
 	
 	@Column(name = "regdate")
